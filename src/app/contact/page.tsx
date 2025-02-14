42ddff5a3bd2710371ac3277753325bbf8e99785
Piyush Kumar <piyushbagoria018@gmail.com>
2025-02-14 11:17:14 +0530

    fixes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,7 @@ export default function ContactPage() {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
       {/* Background Animation */}
-      {/* <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-10" /> */}
+      <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-10" />
 
       <div className="max-w-2xl mx-auto p-4 relative z-10 text-center">
         {/* Page Title */}
