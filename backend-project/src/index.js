9837623107232a1dd44c28609c2da528aa65f7a1
Piyush Kumar <piyushbagoria018@gmail.com>
2025-06-26 16:13:54 +0530

    Refactor server port handling to remove default value in index.js

--- backend-project/src/index.js
+++ backend-project/src/index.js
@@ -6,8 +6,8 @@ dotenv.config({ path: "./.env" });
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
     });
   })
   .catch((error) => console.error("Error connecting to MongoDB", error));
